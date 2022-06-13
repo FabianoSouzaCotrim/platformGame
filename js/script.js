951afa3e5d8f23aafe0d19ac0f96d43a1e27dde5
fabiano souza <fabianokurashi@gmail.com>
2022-06-13 13:52:24 -0300

    melhorias js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const mrRobot = document.querySelector('.mrRobot')
+const obstaculo = document.querySelector('.obstaculo')
+
 console.log(mrRobot);
 const jump = ()=>{
     mrRobot.classList.add('jump')
@@ -6,4 +8,20 @@ const jump = ()=>{
         mrRobot.classList.remove('jump')
     },500)
 }
+
+const loop =setInterval(() => {
+    const obstaculoPosition = obstaculo.offsetLeft
+    const mrRobotPosition = +window.getComputedStyle(mrRobot).bottom
+    .replace('px', '')
+    if(obstaculoPosition <= 120 && pipePosition > 0 && mrRobotPosition<80){
+        obstaculo.style.animation = 'none'
+        obstaculo.style.bottom = `${obstaculo}px`
+        mrRobot.src = 'css/sprites/death/death.gif'
+    }
+}, 10);
+
+
+
+
 document.addEventListener('keydown', jump)
+
